256dec6b5d50f70fb7c2b374e5ac4f8d12df01f1
adanamanya <adanamanyaa@gmail.com>
2026-01-15 13:48:31 +0700

    refactor: migrate shield and transfer demos to use reusable components

--- app/src/components/demo/index.ts
+++ app/src/components/demo/index.ts
@@ -3,3 +3,4 @@ export { DemoPageHeader } from './DemoPageHeader';
 export { InfoCard } from './InfoCard';
 export { DemoButton } from './DemoButton';
 export { TxLink } from './TxLink';
+export { PrivacyVisualizer } from './PrivacyVisualizer';
